49486087494b846c1616af658b65bc7e361f5cb4
Kavindu Perera <kavinduperera2010@gmail.com>
2024-12-24 21:19:57 +0530

    some refactoring

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,24 +1,23 @@
-const staticChangeLog = "changelog-pwa-v1"
+const staticChangeLog = "changelog-pwa-v1";
 const assets = [
     "/",
     "/changelog.html",
     "/changelog.png",
-    "/favicon.ico",
-    "/changelog-data.json",
-]
+    "/favicon.ico"
+];
 
 self.addEventListener("install", installEvent => {
     installEvent.waitUntil(
         caches.open(staticChangeLog).then(cache => {
-            cache.addAll(assets)
+            cache.addAll(assets);
         })
-    )
-})
+    );
+});
 
 self.addEventListener("fetch", fetchEvent => {
     fetchEvent.respondWith(
         caches.match(fetchEvent.request).then(cacheResponse => {
-            return cacheResponse || fetch(fetchEvent.request)
+            return cacheResponse || fetch(fetchEvent.request);
         })
-    )
-})
+    );
+});
